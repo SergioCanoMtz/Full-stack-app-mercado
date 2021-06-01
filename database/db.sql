aebecb4ef01da62c5b2601352624cad485672421
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-06-01 15:59:34 -0500

    Cambios en la base de datos(rows)

--- database/db.sql
+++ database/db.sql
@@ -2,28 +2,28 @@ create database database_link;
 
 use database_link;
 
-create table users(
+create table user(
     id int(11) not null primary key,
     username varchar(20) not null,
     password varchar(60) not null,
     fullname varchar(100) not null
 )
 
-alter table users
+alter table user
     modify id int(11) not null auto_increment, auto_increment = 2;
 
-describe users;
+describe user;
 
 --links table
 
-create table link (
+create table producto (
     id int(11) not null,
     title varchar(150) not null,
     url varchar(200) not null,
     description text,
     created_at timestamp not null default current_timestamp,
     user_id int(11),
-    constraint fk_user foreign key (user_id) references users(id)
+    constraint fk_user foreign key (user_id) references user(id)
 );
 
 alter table link
