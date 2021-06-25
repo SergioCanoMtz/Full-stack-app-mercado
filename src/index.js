0b57fe72bc0c995452c8473f6c0289163be3c722
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-06-24 23:51:30 -0500

    index js

--- src/index.js
+++ src/index.js
@@ -55,8 +55,6 @@ app.use(require('./routes'));
 app.use(require('./routes/authentication'));
 app.use('/links',require('./routes/links'));
 
-
-
 //public
 app.use(express.static(path.join(__dirname, 'public')));
 
